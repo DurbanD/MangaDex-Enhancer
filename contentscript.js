d46c86a94e0b342f1affa977edce79a9a966994b
DurbanD <andrew@durbantech.com>
2022-07-16 21:48:31 -0400

    encapsulated connection and messaging logic into functions

--- contentscript.js
+++ contentscript.js
@@ -4,35 +4,37 @@ class View {
     }
 }
 
+// Handle recieved messages
+let listenForConnectionMessages = function(port) {
+    port.onMessage.addListener(function(msg) {
+        console.log(`Message Recieved over port ${port.name}: \n`, msg)
+      });
+}
 
-// Send Messages to background.js
-// chrome.runtime.sendMessage({type: "getName"}, function(response) {
-//     console.log('getName Response: ', response);
-// });
-
-let openPort = function() {
+let openConnection = function() {
     let name, port
     chrome.runtime.sendMessage({type: "getName"}, function(response) {
         name = response.msg
     });
     port = chrome.runtime.connect({name:name})
+
     return port
 }
-let port = openPort()
-// Setup a connection to background.js to request and recieve async api calls
-// let port = chrome.runtime.connect({name: "API_Controller"});
-// Handle recieved messages
-port.onMessage.addListener(function(msg) {
-  console.log(msg)
-});
-console.log(port)
-
-port.postMessage({type:'idGet', idList:['371bb8db-b84a-495e-bdb6-a744da3c2f5e']});
-// port.postMessage({type:'lookupHistory'});
 
 // Handle recieving messages from background.js (Sync)
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender);
-    }
-);
+let listenForMessages = function () {
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+          console.log(sender);
+        }
+    );
+}
+
+let connect = function () {
+    let port = openConnection()
+    listenForConnectionMessages(port)
+    listenForMessages()
+    return port
+}
+let port = connect()
+port.postMessage({type:'idGet', idList:['371bb8db-b84a-495e-bdb6-a744da3c2f5e']});
