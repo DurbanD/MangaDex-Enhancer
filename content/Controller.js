02bff22d30d1d44d5682bade2529877828add324
DurbanD <andrew@durbantech.com>
2022-07-24 18:55:33 -0400

    Structured the Manga info better and now it only populates information for manga seen by view

--- content/Controller.js
+++ content/Controller.js
@@ -1,3 +1,15 @@
+class Manga {
+    constructor(id, container = null) {
+        this.id = id
+        this.container = container
+        this.info = null
+        this.user = {
+            read : [],
+            rating: null
+        }
+    }
+}
+
 class MangaInfo {
     constructor(container, fullInfo, userInfo) {
         this.container = container
@@ -42,8 +54,9 @@ export default class Controller {
     setContainers(view) {
         for (let card of view.cards) {
             let id = view.getCardID(card)
-            if (this.dataMap.has(id)) this.dataMap.get(id).container = card
-            else this.dataMap.set(id, new MangaInfo(card, null, {read:[], rating:null}))
+            if (Controller.dataMap.has(id)) Controller.dataMap.get(id).container = card
+            // else this.dataMap.set(id, new MangaInfo(card, null, {read:[], rating:null}))
+            else Controller.dataMap.set(id, new Manga(id, card))
         }
     }
 
@@ -53,36 +66,62 @@ export default class Controller {
 
             switch (msg.type){
                 case "get_manga_response":
-                    if (msg.body.result !== "ok") throw Error('get_manga Failed: \n', msg)
-                    else {
+                    if (msg.body === null) break
+                    if (msg.body.result === "ok") {
+                        console.log(msg)
                         for (let res of msg.body.data) {
-                            if (Controller.dataMap.has(res.id)) Controller.dataMap.get(res.id).fullInfo = res
-                            else Controller.dataMap.set(res.id, new MangaInfo(null, res, {read:[], rating:null}))
+                            if (Controller.dataMap.has(res.id)) Controller.dataMap.get(res.id).info = res
+                            // else {
+                            //     let newManga = new Manga(res.id)
+                            //     newManga.info = res
+                            //     Controller.dataMap.set(res.id, newManga)
+                            // }
+                            // if (Controller.dataMap.has(res.id)) Controller.dataMap.get(res.id).fullInfo = res
+                            // else Controller.dataMap.set(res.id, new MangaInfo(null, res, {read:[], rating:null}))
                         }
                     } 
                     break
                 case 'get_rating_response':
-                    for (let id of Object.keys(msg.body.ratings)) {
-                        if (Controller.dataMap.has(id)) Controller.dataMap.get(id).userInfo.rating = msg.body.ratings[id].rating
-                        else Controller.dataMap.set(id, new MangaInfo(null,null, {read: null, rating:null}))
-                    }
+                    if (!msg.body) break
+                    if (msg.body.result === "ok") {
+                        for (let id of Object.keys(msg.body.ratings)) {
+                            let mangaRating = msg.body.ratings[id].rating
+                            if (Controller.dataMap.has(id)) Controller.dataMap.get(id).user.rating = mangaRating
+                            // else {
+                            //     let newManga = new Manga(id)
+                            //     newManga.user.rating = mangaRating
+                            //     Controller.dataMap.set(id, newManga)
+                            // }
+                            // if (Controller.dataMap.has(id)) Controller.dataMap.get(id).userInfo.rating = msg.body.ratings[id].rating
+                            // else Controller.dataMap.set(id, new MangaInfo(null,null, {read: null, rating:msg.body.ratings[id].rating}))
+                        }
+                    }                    
                     break
                 case 'get_read_response':
                     if (msg.body === null) break
-                    if (msg.body.result !== "ok") throw Error('get_read failed! \n', msg)
-                    else {
+                    console.log(msg)
+                    if (msg.body.result === "ok") {
                         let readChapters = msg.body.data
                         while (readChapters.length > 0) Controller.sendMessage('get_chapter', {idList: readChapters.splice(0,100)})
                     }
                     break
                 case 'get_chapter_response':
-                    if (msg.body.result !== "ok") throw Error('get_chapter Failed: \n', msg)
-                    else {
+                    if (msg.body === null) break
+                    console.log(msg)
+                    if (msg.body.result === "ok") {
                         for (let res of msg.body.data) {
                             let id = ''
-                            for (let relationship of res.relationships) if (relationship.type === 'manga') id = relationship.id
-                            if (Controller.dataMap.has(id)) Controller.dataMap.get(id).userInfo.read.push(res)
-                            else Controller.dataMap.set(id, new MangaInfo(null,null, {read: res, rating:null}))
+                            for (let relationship of res.relationships) if (relationship.type === 'manga') {
+                                id = relationship.id
+                                if (Controller.dataMap.has(id)) Controller.dataMap.get(id).user.read.push(res)
+                                // else {
+                                //     let newManga = new Manga(id)
+                                //     newManga.user.read = [res]
+                                //     Controller.dataMap.set(id, newManga)
+                                // }
+                            }
+                            // if (Controller.dataMap.has(id)) Controller.dataMap.get(id).userInfo.read.push(res)
+                            // else Controller.dataMap.set(id, new MangaInfo(null,null, {read: [res], rating:null}))
                         }
                     }
                     break
@@ -137,9 +176,9 @@ export default class Controller {
         this.setContainers(view)
         this.setTokens(this.getTokens(view))
 
-        for (let key of this.dataMap.keys()) if (this.dataMap.get(key).fullInfo === null) newManga.push(key)
+        for (let key of this.dataMap.keys()) if (this.dataMap.get(key).info === null) newManga.push(key)
 
-        Controller.sendMessage('get_manga', { idList: newManga})
+        if (newManga.length > 0) Controller.sendMessage('get_manga', { idList: newManga})
         if (this.authTokens.session !== null) {
             Controller.sendMessage('get_read', { idList: newManga, token: this.authTokens.session})
             Controller.sendMessage('get_rating', { idList: newManga, token: this.authTokens.session})
